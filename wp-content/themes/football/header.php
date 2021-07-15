3bcff5882f32fed7f9ffcf9119edb438ca6e6527
King Of The East <dennis.kiptoo@programmer.net>
2021-07-15 11:34:07 +0300

    template

--- wp-content/themes/football/header.php
+++ wp-content/themes/football/header.php
@@ -15,7 +15,7 @@ $logo = wp_get_attachment_image_src( $custom_logo_id , 'full' );
 ?>
 <div class="navbar sticky-top nav-light justify-content-center d-none d-md-block">
     <a class="navbar-brand  px-2 " href="<?= get_home_url(); ?>">
-        <img src="<?=$logo[0]; ?>"   alt="Football Mania logo">
+        <img src="<?=$logo[0]; ?>" height="100"  alt="Football Mania logo">
     </a>
 </div>
 <nav class="navbar navbar-expand-md sticky-top  navbar-dark py-0" style="background: #000000;">
