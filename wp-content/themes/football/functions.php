7e82d29f177d2c6bc90ddc9927932de4758f9b5a
King Of The East <dennis.kiptoo@programmer.net>
2021-07-17 17:29:53 +0300

    sidebar

--- wp-content/themes/football/functions.php
+++ wp-content/themes/football/functions.php
@@ -17,10 +17,16 @@ if (function_exists('add_theme_support'))
     add_theme_support( 'menus' );
     add_theme_support( 'title-tag' );
     add_theme_support('post-thumbnails');
-    add_theme_support( 'custom-background', apply_filters( 'bam_custom_background_args', array(
-        'default-color' => '#ffffff',
+    add_theme_support( "custom-header" );
+
+    // Add default posts and comments RSS feed links to head.
+    add_theme_support( 'automatic-feed-links' );
+
+    add_theme_support( 'custom-background', apply_filters( 'consultio_custom_background_args', array(
+        'default-color' => 'ffffff',
         'default-image' => '',
     ) ) );
+    add_theme_support( 'customize-selective-refresh-widgets' );
     add_theme_support( 'html5', array(
         'search-form',
         'comment-form',
