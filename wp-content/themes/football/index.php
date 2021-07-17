337d7300dac139603794f3bd57a33d6890c86429
King Of The East <dennis.kiptoo@programmer.net>
2021-07-17 18:41:33 +0300

    sidebar

--- wp-content/themes/football/index.php
+++ wp-content/themes/football/index.php
@@ -10,15 +10,12 @@ get_header();
 ?>
 <!-- Start Page Title Area -->
 <div class="row my-3">
-
-            <div class="container">
-                <nav aria-label="breadcrumb">
-                    <ol class="breadcrumb">
-                        <li class="breadcrumb-item"><a href="#">Home</a></li>
-                        <li class="breadcrumb-item active" aria-current="page">Library</li>
-                    </ol>
-                </nav>
-            </div>
+    <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="#">Home</a></li>
+            <li class="breadcrumb-item active" aria-current="page">Library</li>
+        </ol>
+    </nav>
 
 </div>
 <!-- End Page Title Area -->
