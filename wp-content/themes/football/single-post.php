aacb02f2823a34f911fbea388400e0d5357723b2
King Of The East <dennis.kiptoo@programmer.net>
2021-07-17 22:57:24 +0300

    template

--- wp-content/themes/football/single-post.php
+++ wp-content/themes/football/single-post.php
@@ -113,7 +113,7 @@ if(have_posts() & is_singular())
 				
 			';
     if ( comments_open() || get_comments_number() ) :
-        comments_template('/comments.php');
+        comments_template('./comments.php');
     endif;
     endwhile;
     wp_reset_postdata();
