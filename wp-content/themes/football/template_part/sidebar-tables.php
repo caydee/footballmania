2dda0d3a117fd8fa0df85e2eb5aa674e628a41b1
King Of The East <dennis.kiptoo@programmer.net>
2021-07-17 18:04:22 +0300

    sidebar

---
+++ wp-content/themes/football/template_part/sidebar-tables.php
@@ -0,0 +1,410 @@
+<div class="card box-shadow mb-3  ">
+    <div class="card-header bg-dark text-white">
+        <strong>TODAYS TOP GAMES</strong>
+    </div>
+    <table class="table table-sm mb-0 standings">
+        <tbody>
+        <tr>
+            <td class="align-middle text-right" ><img src="https://ssl.gstatic.com/onebox/media/sports/logos/z44l-a0W1v5FmgPnemV6Xw_48x48.png" alt="" class="team-icon">Man. City</td>
+            <td class="align-middle text-center bg-light small">15:00hrs <br> EPL</td>
+            <td class="align-middle"><img src="https://ssl.gstatic.com/onebox/media/sports/logos/udQ6ns69PctCv143h-GeYw_48x48.png" alt="" class="team-icon">Man. United</td>
+        </tr>
+        <tr>
+            <td class="align-middle text-right" ><img src="https://ssl.gstatic.com/onebox/media/sports/logos/z44l-a0W1v5FmgPnemV6Xw_48x48.png" alt="" class="team-icon">Man. City</td>
+            <td class="align-middle text-center  bg-light small">15:00hrs <br> Champions</td>
+            <td class="align-middle"><img src="https://ssl.gstatic.com/onebox/media/sports/logos/udQ6ns69PctCv143h-GeYw_48x48.png" alt="" class="team-icon">Man. United</td>
+        </tr>
+        <tr>
+            <td class="align-middle text-right" ><img src="https://ssl.gstatic.com/onebox/media/sports/logos/z44l-a0W1v5FmgPnemV6Xw_48x48.png" alt="" class="team-icon">Man. City</td>
+            <td class="align-middle text-center  bg-light small">15:00hrs <br>World Cup</td>
+            <td class="align-middle"><img src="https://ssl.gstatic.com/onebox/media/sports/logos/udQ6ns69PctCv143h-GeYw_48x48.png" alt="" class="team-icon">Man. United</td>
+        </tr>
+        <tr>
+            <td class="align-middle text-right" ><img src="https://ssl.gstatic.com/onebox/media/sports/logos/z44l-a0W1v5FmgPnemV6Xw_48x48.png" alt="" class="team-icon">Man. City</td>
+            <td class="align-middle text-center bg-light small">15:00hrs <br> Africa Cup</td>
+            <td class="align-middle"><img src="https://ssl.gstatic.com/onebox/media/sports/logos/udQ6ns69PctCv143h-GeYw_48x48.png" alt="" class="team-icon">Man. United</td>
+        </tr>
+        <tr>
+            <td class="align-middle text-right" ><img src="https://ssl.gstatic.com/onebox/media/sports/logos/z44l-a0W1v5FmgPnemV6Xw_48x48.png" alt="" class="team-icon">Man. City</td>
+            <td class="align-middle text-center  bg-light small">15:00hrs <br> Europa</td>
+            <td class="align-middle"><img src="https://ssl.gstatic.com/onebox/media/sports/logos/udQ6ns69PctCv143h-GeYw_48x48.png" alt="" class="team-icon">Man. United</td>
+        </tr>
+
+        </tbody>
+    </table>
+    <a href="#" class="btn btn-primary btn btn-sm block">SHOW MORE</a>
+</div>
+<div class="card box-shadow mb-3  ">
+    <div class="card-header bg-dark text-white">
+        <strong>RESULTS</strong>
+    </div>
+    <table class="table  mb-0 standings">
+        <tbody>
+        <tr>
+            <td class="align-middle text-right" ><img src="https://ssl.gstatic.com/onebox/media/sports/logos/z44l-a0W1v5FmgPnemV6Xw_48x48.png" alt="" class="team-icon">Man. City</td>
+            <td class="align text-center"><strong>2-1</strong></td>
+            <td class="align-middle "><img src="https://ssl.gstatic.com/onebox/media/sports/logos/udQ6ns69PctCv143h-GeYw_48x48.png" alt="" class="team-icon">Man. United</td>
+        </tr>
+        <tr>
+            <td class="align-middle text-right" ><img src="https://ssl.gstatic.com/onebox/media/sports/logos/z44l-a0W1v5FmgPnemV6Xw_48x48.png" alt="" class="team-icon">Man. City</td>
+            <td class="align text-center"><strong>2-1</strong></td>
+            <td class="align-middle "><img src="https://ssl.gstatic.com/onebox/media/sports/logos/udQ6ns69PctCv143h-GeYw_48x48.png" alt="" class="team-icon">Man. United</td>
+        </tr>
+        <tr>
+            <td class="align-middle text-right" ><img src="https://ssl.gstatic.com/onebox/media/sports/logos/z44l-a0W1v5FmgPnemV6Xw_48x48.png" alt="" class="team-icon">Man. City</td>
+            <td class="align text-center"><strong>2-1</strong></td>
+            <td class="align-middle "><img src="https://ssl.gstatic.com/onebox/media/sports/logos/udQ6ns69PctCv143h-GeYw_48x48.png" alt="" class="team-icon">Man. United</td>
+        </tr>
+        <tr>
+            <td class="align-middle text-right" ><img src="https://ssl.gstatic.com/onebox/media/sports/logos/z44l-a0W1v5FmgPnemV6Xw_48x48.png" alt="" class="team-icon">Man. City</td>
+            <td class="align text-center"><strong>2-1</strong></td>
+            <td class="align-middle "><img src="https://ssl.gstatic.com/onebox/media/sports/logos/udQ6ns69PctCv143h-GeYw_48x48.png" alt="" class="team-icon">Man. United</td>
+        </tr>
+        <tr>
+            <td class="align-middle text-right" ><img src="https://ssl.gstatic.com/onebox/media/sports/logos/z44l-a0W1v5FmgPnemV6Xw_48x48.png" alt="" class="team-icon">Man. City</td>
+            <td class="align text-center"><strong>2-1</strong></td>
+            <td class="align-middle"><img src="https://ssl.gstatic.com/onebox/media/sports/logos/udQ6ns69PctCv143h-GeYw_48x48.png" alt="" class="team-icon">Man. United</td>
+        </tr>
+        </tbody>
+    </table>
+    <a href="#" class="btn btn-primary btn-sm btn block">SHOW MORE</a>
+</div>
+<div class="card box-shadow mb-3  ">
+    <div class="card-header bg-dark text-white">
+        <strong>English Premier league standings</strong>
+    </div>
+    <table class="table table-sm mb-0 standings">
+        <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Club</th>
+            <th scope="col">GP</th>
+            <th scope="col">W</th>
+            <th scope="col">D</th>
+            <th scope="col">L</th>
+            <th scope="col">Pts</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <th scope="row">1</th>
+            <td ><img src="https://ssl.gstatic.com/onebox/media/sports/logos/z44l-a0W1v5FmgPnemV6Xw_48x48.png" alt="" class="team-icon">Man. City</td>
+            <td>29</td>
+            <td>25</td>
+            <td>25</td>
+            <td>25</td>
+            <td>25</td>
+
+        </tr>
+        <tr>
+            <th scope="row">2</th>
+            <td><img src="https://ssl.gstatic.com/onebox/media/sports/logos/udQ6ns69PctCv143h-GeYw_48x48.png" alt="" class="team-icon">Man. United</td>
+            <td>29</td>
+            <td>25</td>
+            <td>25</td>
+            <td>25</td>
+            <td>25</td>
+        </tr>
+        <tr>
+            <th scope="row">3</th>
+            <td><img src="https://ssl.gstatic.com/onebox/media/sports/logos/0iShHhASp5q1SL4JhtwJiw_48x48.png" alt="" class="team-icon">Liverpool</td>
+            <td>29</td>
+            <td>25</td>
+            <td>25</td>
+            <td>25</td>
+            <td>25</td>
+        </tr>
+        <tr>
+            <th scope="row">4</th>
+            <td><img src="https://ssl.gstatic.com/onebox/media/sports/logos/k3Q_mKE98Dnohrcea0JFgQ_48x48.png" alt="" class="team-icon">Tottendham</td>
+            <td>29</td>
+            <td>25</td>
+            <td>25</td>
+            <td>25</td>
+            <td>25</td>
+        </tr>
+        <tr>
+            <th scope="row">5</th>
+            <td><img src="https://ssl.gstatic.com/onebox/media/sports/logos/fhBITrIlbQxhVB6IjxUO6Q_48x48.png" alt="" class="team-icon">Chelsea</td>
+            <td>29</td>
+            <td>25</td>
+            <td>25</td>
+            <td>25</td>
+            <td>25</td>
+        </tr>
+        <tr>
+            <th scope="row">6</th>
+            <td><img src="https://ssl.gstatic.com/onebox/media/sports/logos/4us2nCgl6kgZc0t3hpW75Q_48x48.png" alt="" class="team-icon">Arsenal</td>
+            <td>29</td>
+            <td>25</td>
+            <td>25</td>
+            <td>25</td>
+            <td>25</td>
+        </tr>
+        <tr>
+            <th scope="row">7</th>
+            <td><img src="https://ssl.gstatic.com/onebox/media/sports/logos/teLLSaMXim_8BA1d93sMng_48x48.png" alt="" class="team-icon">Burnley FC</td>
+            <td>29</td>
+            <td>25</td>
+            <td>25</td>
+            <td>25</td>
+            <td>25</td>
+        </tr>
+        <tr>
+            <th scope="row">8</th>
+            <td><img src="https://ssl.gstatic.com/onebox/media/sports/logos/UDYY4FSlty6fXFBzvFfcyw_48x48.png" alt="" class="team-icon">Leicester City</td>
+            <td>29</td>
+            <td>25</td>
+            <td>25</td>
+            <td>25</td>
+            <td>25</td>
+        </tr>
+        <tr>
+            <th scope="row">9</th>
+            <td><img src="https://ssl.gstatic.com/onebox/media/sports/logos/lHr1L31i7aJS-O8tUdHOIQ_48x48.png" alt="" class="team-icon">Watford</td>
+            <td>29</td>
+            <td>25</td>
+            <td>25</td>
+            <td>25</td>
+            <td>25</td>
+        </tr>
+        <tr>
+            <th scope="row">10</th>
+            <td><img src="https://ssl.gstatic.com/onebox/media/sports/logos/EKIe0e-ZIphOcfQAwsuEEQ_48x48.png" alt="" class="team-icon">Brighton</td>
+            <td>29</td>
+            <td>25</td>
+            <td>25</td>
+            <td>25</td>
+            <td>25</td>
+        </tr>
+
+        </tbody>
+    </table>
+    <a href="#" class="btn btn-primary btn btn-sm block">SHOW MORE</a>
+</div>
+<div class="card box-shadow mb-3  ">
+    <div class="card-header bg-dark text-white">
+        <strong>Kenya Premier league standings</strong>
+    </div>
+    <table class="table table-sm mb-0 standings">
+        <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Club</th>
+            <th scope="col">GP</th>
+            <th scope="col">W</th>
+            <th scope="col">D</th>
+            <th scope="col">L</th>
+            <th scope="col">Pts</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <th scope="row">1</th>
+            <td ><img src="https://ssl.gstatic.com/onebox/media/sports/logos/z44l-a0W1v5FmgPnemV6Xw_48x48.png" alt="" class="team-icon">Man. City</td>
+            <td>29</td>
+            <td>25</td>
+            <td>25</td>
+            <td>25</td>
+            <td>25</td>
+
+        </tr>
+        <tr>
+            <th scope="row">2</th>
+            <td><img src="https://ssl.gstatic.com/onebox/media/sports/logos/udQ6ns69PctCv143h-GeYw_48x48.png" alt="" class="team-icon">Man. United</td>
+            <td>29</td>
+            <td>25</td>
+            <td>25</td>
+            <td>25</td>
+            <td>25</td>
+        </tr>
+        <tr>
+            <th scope="row">3</th>
+            <td><img src="https://ssl.gstatic.com/onebox/media/sports/logos/0iShHhASp5q1SL4JhtwJiw_48x48.png" alt="" class="team-icon">Liverpool</td>
+            <td>29</td>
+            <td>25</td>
+            <td>25</td>
+            <td>25</td>
+            <td>25</td>
+        </tr>
+        <tr>
+            <th scope="row">4</th>
+            <td><img src="https://ssl.gstatic.com/onebox/media/sports/logos/k3Q_mKE98Dnohrcea0JFgQ_48x48.png" alt="" class="team-icon">Tottendham</td>
+            <td>29</td>
+            <td>25</td>
+            <td>25</td>
+            <td>25</td>
+            <td>25</td>
+        </tr>
+        <tr>
+            <th scope="row">5</th>
+            <td><img src="https://ssl.gstatic.com/onebox/media/sports/logos/fhBITrIlbQxhVB6IjxUO6Q_48x48.png" alt="" class="team-icon">Chelsea</td>
+            <td>29</td>
+            <td>25</td>
+            <td>25</td>
+            <td>25</td>
+            <td>25</td>
+        </tr>
+        <tr>
+            <th scope="row">6</th>
+            <td><img src="https://ssl.gstatic.com/onebox/media/sports/logos/4us2nCgl6kgZc0t3hpW75Q_48x48.png" alt="" class="team-icon">Arsenal</td>
+            <td>29</td>
+            <td>25</td>
+            <td>25</td>
+            <td>25</td>
+            <td>25</td>
+        </tr>
+        <tr>
+            <th scope="row">7</th>
+            <td><img src="https://ssl.gstatic.com/onebox/media/sports/logos/teLLSaMXim_8BA1d93sMng_48x48.png" alt="" class="team-icon">Burnley FC</td>
+            <td>29</td>
+            <td>25</td>
+            <td>25</td>
+            <td>25</td>
+            <td>25</td>
+        </tr>
+        <tr>
+            <th scope="row">8</th>
+            <td><img src="https://ssl.gstatic.com/onebox/media/sports/logos/UDYY4FSlty6fXFBzvFfcyw_48x48.png" alt="" class="team-icon">Leicester City</td>
+            <td>29</td>
+            <td>25</td>
+            <td>25</td>
+            <td>25</td>
+            <td>25</td>
+        </tr>
+        <tr>
+            <th scope="row">9</th>
+            <td><img src="https://ssl.gstatic.com/onebox/media/sports/logos/lHr1L31i7aJS-O8tUdHOIQ_48x48.png" alt="" class="team-icon">Watford</td>
+            <td>29</td>
+            <td>25</td>
+            <td>25</td>
+            <td>25</td>
+            <td>25</td>
+        </tr>
+        <tr>
+            <th scope="row">10</th>
+            <td><img src="https://ssl.gstatic.com/onebox/media/sports/logos/EKIe0e-ZIphOcfQAwsuEEQ_48x48.png" alt="" class="team-icon">Brighton</td>
+            <td>29</td>
+            <td>25</td>
+            <td>25</td>
+            <td>25</td>
+            <td>25</td>
+        </tr>
+
+        </tbody>
+    </table>
+    <a href="#" class="btn btn-primary btn btn-sm block">SHOW MORE</a>
+</div>
+<div class="card box-shadow mb-3  ">
+    <div class="card-header bg-dark text-white">
+        <strong>National Super league standings</strong>
+    </div>
+    <table class="table table-sm mb-0 standings">
+        <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Club</th>
+            <th scope="col">GP</th>
+            <th scope="col">W</th>
+            <th scope="col">D</th>
+            <th scope="col">L</th>
+            <th scope="col">Pts</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <th scope="row">1</th>
+            <td ><img src="https://ssl.gstatic.com/onebox/media/sports/logos/z44l-a0W1v5FmgPnemV6Xw_48x48.png" alt="" class="team-icon">Man. City</td>
+            <td>29</td>
+            <td>25</td>
+            <td>25</td>
+            <td>25</td>
+            <td>25</td>
+
+        </tr>
+        <tr>
+            <th scope="row">2</th>
+            <td><img src="https://ssl.gstatic.com/onebox/media/sports/logos/udQ6ns69PctCv143h-GeYw_48x48.png" alt="" class="team-icon">Man. United</td>
+            <td>29</td>
+            <td>25</td>
+            <td>25</td>
+            <td>25</td>
+            <td>25</td>
+        </tr>
+        <tr>
+            <th scope="row">3</th>
+            <td><img src="https://ssl.gstatic.com/onebox/media/sports/logos/0iShHhASp5q1SL4JhtwJiw_48x48.png" alt="" class="team-icon">Liverpool</td>
+            <td>29</td>
+            <td>25</td>
+            <td>25</td>
+            <td>25</td>
+            <td>25</td>
+        </tr>
+        <tr>
+            <th scope="row">4</th>
+            <td><img src="https://ssl.gstatic.com/onebox/media/sports/logos/k3Q_mKE98Dnohrcea0JFgQ_48x48.png" alt="" class="team-icon">Tottendham</td>
+            <td>29</td>
+            <td>25</td>
+            <td>25</td>
+            <td>25</td>
+            <td>25</td>
+        </tr>
+        <tr>
+            <th scope="row">5</th>
+            <td><img src="https://ssl.gstatic.com/onebox/media/sports/logos/fhBITrIlbQxhVB6IjxUO6Q_48x48.png" alt="" class="team-icon">Chelsea</td>
+            <td>29</td>
+            <td>25</td>
+            <td>25</td>
+            <td>25</td>
+            <td>25</td>
+        </tr>
+        <tr>
+            <th scope="row">6</th>
+            <td><img src="https://ssl.gstatic.com/onebox/media/sports/logos/4us2nCgl6kgZc0t3hpW75Q_48x48.png" alt="" class="team-icon">Arsenal</td>
+            <td>29</td>
+            <td>25</td>
+            <td>25</td>
+            <td>25</td>
+            <td>25</td>
+        </tr>
+        <tr>
+            <th scope="row">7</th>
+            <td><img src="https://ssl.gstatic.com/onebox/media/sports/logos/teLLSaMXim_8BA1d93sMng_48x48.png" alt="" class="team-icon">Burnley FC</td>
+            <td>29</td>
+            <td>25</td>
+            <td>25</td>
+            <td>25</td>
+            <td>25</td>
+        </tr>
+        <tr>
+            <th scope="row">8</th>
+            <td><img src="https://ssl.gstatic.com/onebox/media/sports/logos/UDYY4FSlty6fXFBzvFfcyw_48x48.png" alt="" class="team-icon">Leicester City</td>
+            <td>29</td>
+            <td>25</td>
+            <td>25</td>
+            <td>25</td>
+            <td>25</td>
+        </tr>
+        <tr>
+            <th scope="row">9</th>
+            <td><img src="https://ssl.gstatic.com/onebox/media/sports/logos/lHr1L31i7aJS-O8tUdHOIQ_48x48.png" alt="" class="team-icon">Watford</td>
+            <td>29</td>
+            <td>25</td>
+            <td>25</td>
+            <td>25</td>
+            <td>25</td>
+        </tr>
+        <tr>
+            <th scope="row">10</th>
+            <td><img src="https://ssl.gstatic.com/onebox/media/sports/logos/EKIe0e-ZIphOcfQAwsuEEQ_48x48.png" alt="" class="team-icon">Brighton</td>
+            <td>29</td>
+            <td>25</td>
+            <td>25</td>
+            <td>25</td>
+            <td>25</td>
+        </tr>
+
+        </tbody>
+    </table>
+    <a href="#" class="btn btn-primary btn btn-sm block">SHOW MORE</a>
+</div>
